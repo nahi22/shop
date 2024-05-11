ee43cbcc986de991e3abc7d23bf861b7572e6477
liana <liana.tsaturyan.a@gmail.com>
2024-05-11 21:10:22 +0400

    small fixing

--- admin/updeat.php
+++ admin/updeat.php
@@ -29,7 +29,7 @@
                 $new_name = $_POST['name'];
                 $new_price = $_POST['price'];
                 $imgUrl = $row['img'];
-                if (isset($_FILES["fileToUpload"]) && isset($_FILES["fileToUpload"]["tmp_name"])) {
+                if (isset($_FILES["fileToUpload"]) && isset($_FILES["fileToUpload"]["tmp_name"]) && $_FILES["fileToUpload"]["tmp_name"] != null) {
                   $target_dir = "../images/products/";
                   $uploadOk = 1;
 
