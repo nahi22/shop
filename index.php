0ebf68a129572e0eac0e93904ef0ea5f27f26e42
liana <liana.tsaturyan.a@gmail.com>
2024-05-11 20:38:27 +0400

    small fixings

--- index.php
+++ index.php
@@ -6,234 +6,216 @@
     <link rel="stylesheet" href="./home/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Document</title>
+    <title>Jewelry Shop</title>
 
 </head>
 <body>
-        <div class="skizb">
-            <img class="logo" src="./home/Barbie_Logo.svg.png" alt="Logo" width="218px" height="76px" >
-            <div class="aj">
-              <a href="support"><i class="fa-solid fa-phone-volume" style="color: #fe16b4;"></i> Support</a>
-              <a href="account"><i class="fa-solid fa-user" style="color:#fe16b4;"></i> Account</a>
-            </div>
-          </div>
-        <div class="menu">
-            <ul>
-              <li><a href="#home">Hot deals</a></li>
-              <li><a href="#news">Shop</a></li>
-              <li><a href="#hone">Home</a></li>
-              <li><a href="#about">about</a></li>
-              <li><a href="#page">page</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          <div class="voronel">
-              <input class="poisk" type="text">
-              <div class="incon">
-              <i class="fa-solid fa-magnifying-glass" style="color: #fe16b4;"></i>
-              </div>
-              <div class="incon1">
-                <a href="ADT"><i class="fa-regular fa-heart"></i> Add to Wislist</a>
-              </div>
-              <div class="incon2">
-                <i class="fa-solid fa-cart-shopping" style="color: #fe16b4;"></i>
-            </div>
-            </div>
-          </div>
+    <div class="skizb">
+        <img class="logo" src="./home/logo.png" alt="Logo" style="width: 150px;" />
+        <div class="aj">
+            <a href="support"><i class="fa-solid fa-phone-volume" style="color: #fe16b4;"></i> Support</a>
+            <a href="account"><i class="fa-solid fa-user" style="color: #fe16b4;"></i> Account</a>
         </div>
-          <section>
-            <div class="f7">
-        <div class="vandak">
-          <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-          <h4>Apple</h4>
-          <p>&#36;35</p>
-        <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-        </div>
-        <div class="vandak">
-          <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-          <h4>Banana</h4>
-          <p>&#36;35</p>
-        <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-        </div>
-        <div class="vandak">
-          <img src="./home/nkar1.jpg" alt=" nkar" width="288px" height="311px">
-          <h4>Chocholate cookies</h4>
-          <p>&#36;35</p>
-        <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-        </div>
-              <div class="vandak">
-                <img src="./home/reklam3.jpg" alt="reklam3" width="100%" height="100%">
-              </div>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div class="voronel">
+            <input class="poisk" type="text" />
+            <div class="incon">
+                <i class="fa-solid fa-magnifying-glass" style="color: #fe16b4;"></i>
             </div>
-          </section>
-          <section>
-            <div class="f7">
-              <div class="vandak">
-                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-                <h4>Chocholate Spread</h4>
+        </div>
+    </div>
+    <section>
+        <div class="f7">
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px" />
+                <h4>Gold Ring</h4>
                 <p>&#36;35</p>
-              <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-              </div>
-              <div class="vandak">
-                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-                <h4>Cooling Oli</h4>
-                <p>&#36;35</p>
-              <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-              </div>
-              <div class="vandak">
-                <img src="./home/nkar1.jpg" alt=" nkar" width="288px" height="311px">
-                <h4>Easter-Bread</h4>
-                <p>&#36;35</p>
-              <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-              </div>
-                    <div class="vandak">
-                      <img src="./home/reklam3.jpg" alt="reklam3" width="100%" height="100%">
-                    </div>
-                  </div>
-                </section>
-                <div class="f7">
-                  <div class="vandak">
-                    <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-                    <h4>Fresh Bread</h4>
-                    <p>&#36;35</p>
-                  <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-                  </div>
-                  <div class="vandak">
-                    <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px">
-                    <h4>Lemon</h4>
-                    <p>&#36;35</p>
-                  <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-                  </div>
-                  <div class="vandak">
-                    <img src="./home/nkar1.jpg" alt=" nkar" width="288px" height="311px">
-                    <h4>Mix Fruits</h4>
-                    <p>&#36;35</p>
-                  <button class="btn"><i class="fa-solid fa-cart-shopping" style="color:white;"></i>Add to Cards</button>
-                  </div>
-                        <div class="vandak">
-                          <img src="./home/reklam3.jpg" alt="reklam3" width="100%" height="100%">
-                        </div>
-                      </div>
-                    </section>
-            <div class="s1">
-            <div>
-                <img src="./home/ice-cream.jpg" alt="ice-cream" width="718px" height="513px">
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
             </div>
-                <div class="d2">
-                    <div class="d3">
-                        <p>Deals of The Manth</p>
-                    </div>
-                    <h1>Buy 1 Ice Cream Tubs<br>And Get 1 Free</h1>
-                    <h5>is simply dummy text of the printing and typesetting<br>industry. Lorem Ipsum has been the industry's standard</h5>
-                  </div>
-                    <div class="circles">
-                      <div class="circle-with-text">
-                        <h1 class="circle">26</h1>
-                        <p class="circle">Days</p>
-                      </div>
-                      <div class="circle-with-text ">
-                      <h1 class="circle">18</h1>
-                      <p class="circle">Hours</p>
-                      </div>
-                      <div class="circle-with-text">
-                        <h1 class="circle">49</h1>
-                        <p class="circle">Minutes</p>
-                      </div>
-                      <div class="circle-with-text">
-                      <h1 class="circle">05</h1>
-                      <p class="circle">Second</p>
-                      </div>
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px" />
+                <h4>Gold Ring</h4>
+                <p>&#36;35</p>
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
+            </div>
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt=" nkar" width="288px" height="311px" />
+                <h4>Silver Ring</h4>
+                <p>&#36;35</p>
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
+            </div>
+            <div class="vandak">
+                <img src="./home/reklam3.jpg" alt="reklam3" width="100%" height="100%" />
+            </div>
+        </div>
+    </section>
 
-                    </div>
+    <section>
+        <div class="f7">
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px" />
+                <h4>Chain</h4>
+                <p>&#36;35</p>
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
             </div>
-        </section>
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt="nkar" width="288px" height="311px" />
+                <h4>Chain 50sm</h4>
+                <p>&#36;35</p>
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
+            </div>
+            <div class="vandak">
+                <img src="./home/nkar1.jpg" alt=" nkar" width="288px" height="311px" />
+                <h4>Chain Silver</h4>
+                <p>&#36;35</p>
+                <button class="btn"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>Add to Cart</button>
+            </div>
+            <div class="vandak">
+                <img src="./home/reklam3.jpg" alt="reklam3" width="100%" height="100%" />
+            </div>
+        </div>
+    </section>
 
-      <footer>
+    <div class="s1">
+        <div>
+            <img src="./home/banner.jpg" alt="ice-cream" width="718px" height="513px" />
+        </div>
+        <div class="d2">
+            <div class="d3">
+                <p>Deals of The Manth</p>
+            </div>
+            <h1>
+                Buy 1 Ring<br />
+                And Get 1 Free
+            </h1>
+            <h5>
+                is simply dummy text of the printing and typesetting<br />
+                industry. Lorem Ipsum has been the industry's standard
+            </h5>
+        </div>
+        <div class="circles">
+            <div class="circle-with-text">
+                <h1 class="circle">26</h1>
+                <p class="circle">Days</p>
+            </div>
+            <div class="circle-with-text">
+                <h1 class="circle">18</h1>
+                <p class="circle">Hours</p>
+            </div>
+            <div class="circle-with-text">
+                <h1 class="circle">49</h1>
+                <p class="circle">Minutes</p>
+            </div>
+            <div class="circle-with-text">
+                <h1 class="circle">05</h1>
+                <p class="circle">Second</p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
         <div class="f3">
             <div class="c1">
-              <i class="fa-solid fa-percent" style="color: #fe16b4;"></i>
+                <i class="fa-solid fa-percent" style="color: #fe16b4;"></i>
             </div>
             <div class="t1">
-              <p>Best Price & Offers</p>
-              <h6>Order $50 Or More</h6>
+                <p>Best Price & Offers</p>
+                <h6>Order $50 Or More</h6>
             </div>
-                <div class="c2">
-                  <i class="fa-solid fa-truck" style="color: #fe16b4;"></i>
-                </div>
-                  <div class="t2">
-                    <p>Free Delivary</p>
-                    <h6>24/7 amazing services</h6>
-                  </div>
-                  <div class="c3">
-                    <i class="fa-solid fa-bag-shopping" style="color: #fe16b4;"></i>
-                  </div>
-                  <div class="t3">
-                    <p>Great Daily Deal</p>
-                    <h6>When you sign up</h6>
-                  </div>
-                  <div class="c4">
-                    <i class="fa-solid fa-percent" style="color:#fe16b4"></i>
-                  </div>
-                  <div class="t4">
-                    <p>Wide Assortment</p>
-                    <h6>Wide Assortment</h6>
-                  </div>
-                    <div class="c5">
-                      <i class="fa-solid fa-percent" style="color:#fe16b4"></i>
-                    </div>
-                    <div class="t5">
-                      <p>Within 30 days</p>
-                      <h6>Within 30 days</h6>
-                    </div>
+            <div class="c2">
+                <i class="fa-solid fa-truck" style="color: #fe16b4;"></i>
+            </div>
+            <div class="t2">
+                <p>Free Delivary</p>
+                <h6>24/7 amazing services</h6>
+            </div>
+            <div class="c3">
+                <i class="fa-solid fa-bag-shopping" style="color: #fe16b4;"></i>
+            </div>
+            <div class="t3">
+                <p>Great Daily Deal</p>
+                <h6>When you sign up</h6>
+            </div>
+            <div class="c4">
+                <i class="fa-solid fa-percent" style="color: #fe16b4;"></i>
+            </div>
+            <div class="t4">
+                <p>Wide Assortment</p>
+                <h6>Wide Assortment</h6>
+            </div>
+            <div class="c5">
+                <i class="fa-solid fa-percent" style="color: #fe16b4;"></i>
+            </div>
+            <div class="t5">
+                <p>Within 30 days</p>
+                <h6>Within 30 days</h6>
+            </div>
         </div>
         <div class="f1">
             <div class="f2">
-                <img class="img2" src="./home/Barbie_Logo.svg.png" alt="Logo" width="218px" height="76px" >
-                <p class="j">Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the <br> industry's standard dummy text ever since the 1500s,<br> when an unknown printer took a galley of type and <br> scrambled it to make a type specimen book. </p>
-                <img class="img3" src="./home/card1.jpg" alt="Name">
+                <img class="img2" src="./home/logo.png" alt="Logo" width="120px"/>
+                <p class="j">
+                    Lorem Ipsum is simply dummy text of the printing and <br />
+                    typesetting industry. Lorem Ipsum has been the <br />
+                    industry's standard dummy text ever since the 1500s,<br />
+                    when an unknown printer took a galley of type and <br />
+                    scrambled it to make a type specimen book.
+                </p>
+                <img class="img3" src="./home/card1.jpg" alt="Name" />
                 <p class="p1">Payment Accepted</p>
             </div>
-                <div class="a1">
-                    <h5 class="h3">Support</h5>
-                    <hr class="l3">
-                  <div class="a4">
-                  <p><a href="TYO">Track Your Order </a></p>
-                  <p><a href="T&C">Terms & Conditions</a></p>
-                  <p><a href="PP">Privacy Policy</a></p>
-                  <p><a href="HC">Help Centre</a></p>
-                  </div>
+            <div class="a1">
+                <h5 class="h3">Support</h5>
+                <hr class="l3" />
+                <div class="a4">
+                    <p><a href="TYO">Track Your Order </a></p>
+                    <p><a href="T&C">Terms & Conditions</a></p>
+                    <p><a href="PP">Privacy Policy</a></p>
+                    <p><a href="HC">Help Centre</a></p>
                 </div>
-              <div class="a2">
+            </div>
+            <div class="a2">
                 <h5 class="h2">Useful Links</h5>
-                <hr class="l2">
+                <hr class="l2" />
                 <p><a href="Home">Home</a></p>
                 <p><a href="Shop">Shop</a></p>
                 <p><a href="About">About</a></p>
                 <p><a href="NA">News & Articles</a></p>
-              </div>
-              <div class="a3">
-                        <h5 class="h1">Useful Links</h5>
-                          <hr class="l1">
-                    <div class="i4">
-                        <i class="fa-solid fa-location-dot" style="color: #fe16b4;"></i>
-                        </div>
-                        <div>
-                        <p class="n1">  Plaza X, XY Floor, XYZ <br> Street, XYZ </p>
-                    </div>
-                        <div class="i5" >
-                            <i class="fa-regular fa-envelope" style="color: #fe16b4;"></i>
-                        </div>
-                            <div>
-                            <p class="n2">yourname@gmail.com</p>
-                            </div>
-                                <div class="i6">
-                                    <i class="fa-solid fa-phone-volume" style="color: #e515ad;"></i>
-                                  </div>
-                                    <div>
-                                    <p class="n3">+37493123456</p>
-                                </div>
+            </div>
+            <div class="a3">
+                <h5 class="h1">Useful Links</h5>
+                <hr class="l1" />
+                <div class="i4">
+                    <i class="fa-solid fa-location-dot" style="color: #fe16b4;"></i>
                 </div>
+                <div>
+                    <p class="n1">
+                        Plaza X, XY Floor, XYZ <br />
+                        Street, XYZ
+                    </p>
+                </div>
+                <div class="i5">
+                    <i class="fa-regular fa-envelope" style="color: #fe16b4;"></i>
+                </div>
+                <div>
+                    <p class="n2">yourname@gmail.com</p>
+                </div>
+                <div class="i6">
+                    <i class="fa-solid fa-phone-volume" style="color: #e515ad;"></i>
+                </div>
+                <div>
+                    <p class="n3">+37493123456</p>
+                </div>
+            </div>
         </div>
-        </div>
-        </footer>
-    </body>
-    </html>
+    </footer>
+
+</body>
+</html>
